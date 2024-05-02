ccdd14aaa8661d42f1be3fee02355376956ff4f7
fmcejudo <fmcejudo@gmail.com>
2024-05-02 10:20:02 +0200

    sample of initialization through scripts

--- docker-initializer/mongo/mongo-init.js
+++ docker-initializer/mongo/mongo-init.js
@@ -1,5 +1,31 @@
 db = db.getSiblingDB('test');
 
 db.createUser({
-
+    user: "test",
+    pwd: "test",
+    roles: [{
+        role: 'readWrite',
+        db: 'test'
+    }]
 });
+
+
+/*
+Increase what you want to initialize when mongo starts up
+ */
+
+
+db.createCollection('test_user');
+
+db.test_user.insertMany([
+    {
+        user: 'DaShaun',
+        company: 'Spring'
+    },
+    {
+        user: 'Josh Long',
+        company: 'Spring'
+    }
+]);
+
+
