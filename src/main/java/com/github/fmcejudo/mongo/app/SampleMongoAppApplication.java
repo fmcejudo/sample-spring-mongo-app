be7a98d62d5b5a2a0cc205fc0b66d094810ea2a6
fmcejudo <fmcejudo@gmail.com>
2024-05-02 08:50:46 +0200

    Initial commit: bare project from start.spring.io

---
+++ src/main/java/com/github/fmcejudo/mongo/app/SampleMongoAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.fmcejudo.mongo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMongoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMongoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fmcejudo/mongo/app/SampleMongoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fmcejudo.mongo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMongoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
